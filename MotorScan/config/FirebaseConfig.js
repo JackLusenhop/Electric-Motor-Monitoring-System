9ee5fdde26c5f2e30c83e3c0db036938576851f8
JackLusenhop <jack.lusenhop@gmail.com>
2024-03-21 13:56:06 -0700

    updated firebase config

--- MotorScan/config/FirebaseConfig.js
+++ MotorScan/config/FirebaseConfig.js
@@ -8,7 +8,15 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {};
+const firebaseConfig = {
+	apiKey: "AIzaSyD1Tq6ihQD7Gj-zt99InaOXYm1VgeqUhZc",
+	authDomain: "motorscan-4eec6.firebaseapp.com",
+	projectId: "motorscan-4eec6",
+	storageBucket: "motorscan-4eec6.appspot.com",
+	messagingSenderId: "438562496838",
+	appId: "1:438562496838:web:2f21c0af235674abc50547",
+	measurementId: "G-461EN2W8FM"
+  };
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
